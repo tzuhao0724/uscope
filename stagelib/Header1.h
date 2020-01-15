fbd594cc24c4b5e3c2b51e44edecd34d48db957b
EXOSOME <EXOSOME@DESKTOP-DOCNIH5>
2020-01-15 17:10:07 +0800

    XYlibaray

---
+++ stagelib/Header1.h
@@ -0,0 +1,31 @@
+#pragma once
+#include<io>
+
+
+namespace xystagelib {
+	class xystage
+	{
+	public:
+		xystage();
+		~xystage();
+		virtual void serial_link(string com, int baud) {
+
+		}
+		virtual void write_function(cmd,x,y) {
+			
+		}
+	private:
+		float x = 0;
+		float y = 0;
+		char cmd;
+	};
+
+	xystage::xystage()
+	{
+	}
+
+	xystage::~xystage()
+	{
+	}
+
+}
